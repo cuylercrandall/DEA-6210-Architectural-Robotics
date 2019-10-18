4370008a76b7e89903eea16a361f6c01e0c657c0
Cuyler Crandall <39314420+cuylercrandall@users.noreply.github.com>
2019-10-17 23:51:31 -0400

    Further updates
    properly implimented servos

--- DEA6210_ConfinedSpace_ButtomsTest/DEA6210_ConfinedSpace_ButtomsTest.ino
+++ DEA6210_ConfinedSpace_ButtomsTest/DEA6210_ConfinedSpace_ButtomsTest.ino
@@ -1,9 +1,15 @@
+#include <Servo.h>
+
 int clockwise = 2;
 int counterclockwise = 4;
 int modeButtom = 3;
 bool cwState = false;
 bool ccwState = false;
 bool modeState = false;
+int pos = 0;
+Servo contServo;
+Servo angServo;
+
 
 void setup() {
   // Initialize serial communication at 9600 bits per second:
@@ -15,12 +21,19 @@ void setup() {
   pinMode(modeButtom, INPUT);
   pinMode(6,OUTPUT);
   digitalWrite(6,HIGH);
-  //pinMode(7,OUTPUT);
-  //digitalWrite(7,LOW);
+  pinMode(7,OUTPUT);
+  digitalWrite(7,LOW);
+
+  // Initializes servo output pins
+  contServo.attach(9);
+  angServo.attach(10);
 }
 
 void loop() {
   // Read input pins
+  cwState = (digitalRead(clockwise)==HIGH);
+  ccwState || (digitalRead(counterclockwise)==HIGH);
+  modeState || (digitalRead(modeButtom)==HIGH);
   for (int i=0; i<10; i++){
     cwState = cwState || (digitalRead(clockwise)==HIGH);
     ccwState = ccwState || (digitalRead(counterclockwise)==HIGH);
@@ -28,23 +41,33 @@ void loop() {
     delay(200);
   }
 
-
+  //Cont. Servo Values: Forward = 180, Reverse = 0, Stop = 90
+  //Ang. Servo Values: 0 or 180 (only)
+  
   // Display output based on pin states
   if ((cwState && ccwState) || (!cwState && !ccwState)){
     // If both directions, do nothing
     Serial.println("Doing nothing");
+    contServo.write(90);
+    
   } else if (cwState && modeState){
     // If clockwise and mode switched, move the secondary servo clockwise
     Serial.println("Secondary clockwise");
+    
   } else if (ccwState && modeState){
     // If counterclockwise and mode switched, move the secondary servo counterclockwise
     Serial.println("Secondary counterclockwise");
+    
   } else if (cwState && !modeState){
     // If clockwise and mode switched, move the primary servo clockwise
     Serial.println("Primary clockwise");
+    contServo.write(180);
+    
   } else if (ccwState && !modeState){
     // If counterclockwise and mode switched, move the primary servo counterclockwise
     Serial.println("Primary counterclockwise");
+    contServo.write(0);
+    
   }
 
   if(cwState){
@@ -58,7 +81,7 @@ void loop() {
   }
   
   // Reset pin states
-  bool cwState = false;
-  bool ccwState = false;
-  bool modeState = false;
+  cwState = false;
+  ccwState = false;
+  modeState = false;
 }
--- DEA6210_ConfinedSpace_ButtomsTest/DEA6210_ConfinedSpace_ServoTest/DEA6210_ConfinedSpace_ServoTest.ino
+++ DEA6210_ConfinedSpace_ButtomsTest/DEA6210_ConfinedSpace_ServoTest/DEA6210_ConfinedSpace_ServoTest.ino
@@ -7,36 +7,49 @@ void setup() {
   //pinMode(contServoPin,OUTPUT);
   //pinMode(angServoPin,OUTPUT);
   pinMode(13, OUTPUT);
-  digitalWrite(13,HIGH);
+  digitalWrite(13, HIGH);
 }
 
 
 void loop() {
-  analogWrite(contServoPin,0);
-  analogWrite(angServoPin,0);
-  delay(1000);
-  analogWrite(contServoPin,30);
-  analogWrite(angServoPin,30);
-  delay(1000);
-  analogWrite(contServoPin,60);
-  analogWrite(angServoPin,60);
-  delay(1000);
-  analogWrite(contServoPin,90);
-  analogWrite(angServoPin,90);
-  delay(1000);
-  analogWrite(contServoPin,120);
-  analogWrite(angServoPin,120);
-  delay(1000);
-  analogWrite(contServoPin,150);
-  analogWrite(angServoPin,150);
-  delay(1000);
-  analogWrite(contServoPin,180);
-  analogWrite(angServoPin,180);
-  delay(1000);
-  analogWrite(contServoPin,210);
-  analogWrite(angServoPin,210);
-  delay(1000);
-  analogWrite(contServoPin,240);
-  analogWrite(angServoPin,240);
-  delay(5000);
+
+  for (pos = 0; pos <= 255; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    analogWrite(angServoPin, pos);             // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 255; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    analogWrite(angServoPin, pos);             // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+
+  /*
+    analogWrite(contServoPin,0);
+    analogWrite(angServoPin,0);
+    delay(1000);
+    analogWrite(contServoPin,30);
+    analogWrite(angServoPin,30);
+    delay(1000);
+    analogWrite(contServoPin,60);
+    analogWrite(angServoPin,60);
+    delay(1000);
+    analogWrite(contServoPin,90);
+    analogWrite(angServoPin,90);
+    delay(1000);
+    analogWrite(contServoPin,120);
+    analogWrite(angServoPin,120);
+    delay(1000);
+    analogWrite(contServoPin,150);
+    analogWrite(angServoPin,150);
+    delay(1000);
+    analogWrite(contServoPin,180);
+    analogWrite(angServoPin,180);
+    delay(1000);
+    analogWrite(contServoPin,210);
+    analogWrite(angServoPin,210);
+    delay(1000);
+    analogWrite(contServoPin,240);
+    analogWrite(angServoPin,240);
+    delay(5000);
+  */
 }
